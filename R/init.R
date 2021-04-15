05e28f289d5b0f34035fc85df0a9ec0718b7815c
asiripanich <17020181+asiripanich@users.noreply.github.com>
2021-04-15 19:15:27 +1000

    init

---
+++ R/init.R
@@ -0,0 +1,46 @@
+#' [renv::init] with extra steps to maintain vscode-R features
+#'
+#' @description
+#'
+#' This function is a wrapper of [renv::init] which adds some extra steps
+#' to maintain vscode-R's sessionWatcher and 'R: Launch RStudio Addin'
+#' features after an [renv] environment gets activated.
+#'
+#' Step 1: Create `dependencies.R` at the path that is determined by
+#' [here::here] and add `library(languageserver); library(jsonlite);
+#' library(rlang)` to the file.
+#'
+#' Step2: Add `library(rstudioapi)` to `dependencies.R`, and then add
+#' `options(vsc.rstudioapi = TRUE)` to `.Rprofile` created by [renv::init].
+#'
+#' @inheritParams renv::init
+#' @importFrom renv init
+#' @export
+init <- function(project = NULL, ..., profile = NULL, settings = NULL,
+                 bare = FALSE, force = FALSE, restart = interactive()) {
+  add_radian_deps()
+  add_rstudioapis()
+
+  renv::init(
+    project = NULL, ..., profile = NULL, settings = NULL,
+    bare = FALSE, force = FALSE, restart = interactive()
+  )
+}
+
+add_radian_deps <- function() {
+  deps <- c("languageserver", "jsonlite", "rlang")
+  for (dep in deps) {
+    cat("library(", dep, ")\n",
+      file = here::here("dependencies.R"),
+      append = TRUE,
+      sep = ""
+    )
+  }
+}
+
+add_rstudioapis <- function() {
+  cat("library(rstudioapi)\n",
+    file = here::here("dependencies.R"),
+    append = TRUE
+  )
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# renvsc
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of renvsc is to enable using the renv package in VSCode without
+breaking the nice features of the vscode-R extension.
+
+## Installation
+
+``` r
+remotes::install_github("asiripanich/renvsc")
+```
